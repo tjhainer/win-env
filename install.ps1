80aa380deba09b2fb381498f5555952590d8ba64
TylerHainer <THainer@cogsdale.com>
2024-10-07 13:53:31 -0300

    add packages for scoop to install

--- install.ps1
+++ install.ps1
@@ -5,6 +5,7 @@ $wingetProgs = @{
 }
 
 # todo user apps - firefox, bitwarden. other apps more optional like vs code, git gui client
+# todo anythink like node, go, etc. for languages
 
 foreach ($prog in $wingetProgs.Keys) {
     winget install -e --id $wingetProgs[$prog] --accept-source-agreements --accept-package-agreements
@@ -16,9 +17,18 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 $scoopProgs = @{
     nvim = 'neovim'
     z = 'zoxide'
+    ripgrep = 'ripgrep'
+    make = 'make'
+    unzip = 'unzip'
+    win32yank = 'win32yank'
+    fd = 'fd'
+    wget = 'wget'
+    mingw = 'mingw'
+    luarocks = 'luarocks'
+    gzip = 'gzip'
 }
 
 # todo add scoop to path?
 foreach ($prog in $scoopProgs.Keys) {
-    scoop install $scoopProgs[$prog]
+    scoop install main/$scoopProgs[$prog]
 }
